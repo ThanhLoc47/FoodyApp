461d8eafa8c69458f353a978fc16671312aa5e4e
ThanhLoc47 <35883152+ThanhLoc47@users.noreply.github.com>
2023-07-15 09:54:50 +0700

    add style splash screen

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -16,4 +16,33 @@ const styles = StyleSheet.create({
     }
 })
 
+
+const backgroundColor = () => {
+    return (
+        <ImageBackground source={SplashImage} style={styles.bg} >
+
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    bg: {
+        flex: 1
+    }
+})
+
+const buttonStyle = () => {
+    return (
+        <ImageBackground source={SplashImage} style={styles.bg} >
+
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    bg: {
+        flex: 1
+    }
+})
+
 export default Splash;
